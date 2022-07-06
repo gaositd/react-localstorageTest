85f907515f271cc8e7da4d4327fa85ad87731a27
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-07-05 22:18:51 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { TaskCreator } from "./components/taskCreator";
 
 function App() {
+  const [taskItem, setTaskItem] = useState([
+    // {
+    //   name: "Tarea 1",
+    //   done: false,
+    // },
+    // {
+    //   name: "Tarea 2",
+    //   done: false,
+    // },
+    // {
+    //   name: "Tarea 3",
+    //   done: false,
+    // },
+  ]);
+
+  function createNewTask(taskName) {
+    // alert(taskName);
+    const encontrado = taskItem.find(item => item.name === taskName);
+
+    if(encontrado){
+      alert("dato insertado anteriormente");
+    }else {
+      setTaskItem([...taskItem, { name: taskName, done: false }]);
+      localStorage.setItem('task', JSON.stringify(taskItem));
+    }
+  };
+
+  useEffect(()=>{
+    let data = localStorage.getItem('task');
+    if(data){
+      console.log(JSON.parse(data));
+    }
+  },[]);
+
+  useEffect(()=>{
+    console.log("cambio")
+  },[taskItem]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskCreator createNewTask={createNewTask} />
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskItem.map((task) => (
+            <tr key={task.name}>
+              <td>{task.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
